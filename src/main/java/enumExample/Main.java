8ba8fd1998db36077cc247a8c483df81321a8120
DerianW <ScratchGobo@gmail.com>
2018-10-12 13:20:15 -0400

    first commit

---
+++ src/main/java/enumExample/Main.java
@@ -0,0 +1,24 @@
+package enumExample ;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /*
+        Enums are like classes. They have fields/properties and they can have methods.
+        Enums are implicitly static and final.
+
+        All properties of enums are implicitly public, static, and final.
+        We write their names with capital letters.
+
+        Properties must be declared before anything else. They are separated by commas.
+
+        We use enums because they can be easier to read and it makes it easier for the compiler to catch errors.
+        We use enums when we need to represent a fixed set of constants.
+
+        Enums can have constructors for their fields.
+
+        All enums have a method called .values() and .toString(), but the toString typically gets overridden.
+         */
+    }
+}
